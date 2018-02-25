6021a7d324b9ba34a7840701d71e4f5e796cd1ac
Fikri <zulfikrizulkiflee@gmail.com>
2018-02-25 22:08:25 +0800

    update

--- js/site.js
+++ js/site.js
@@ -2,6 +2,121 @@
 var calendars = {};
 $(document).ready(function () {
     // Configure/customize these variables.
+    if (localStorage.getItem('MH_user_name') != null) {
+        $.mobile.navigate('#profile');
+    }
+    $('.next1').on('click', function () {
+        if ($('input[name=user_name]').val() != "" && $('input[name=user_age]').val() != "") {
+            localStorage.setItem('MH_user_name', $('input[name=user_name]').val());
+            localStorage.setItem('MH_user_age', $('input[name=user_age]').val());
+            localStorage.setItem('MH_user_gender', $('input[name=gender]').val());
+            if (localStorage.getItem('MH_user_name') != null && localStorage.getItem('MH_user_name') != "" && localStorage.getItem('MH_user_age') != '' && localStorage.getItem('MH_user_age') != null) {
+                $.mobile.navigate('#landing2');
+                $('.profile_picture_name h2').html(localStorage.getItem('MH_user_name'));
+                if (localStorage.getItem('MH_user_gender') == 'male') {
+                    $('.profile_user_age img').attr('src', 'images/masculine.png');
+                }
+                else {
+                    $('.profile_user_age img').attr('src', 'images/femenine.png');
+                }
+                $('.profile_user_age age').html(localStorage.getItem('MH_user_age'));
+            }
+        }
+        else {
+            alert('Sila lengkapkan pendaftaran anda.');
+        }
+    });
+    $('.next2').on('click', function () {
+        if ($('input[name=user_weight]').val() != "" && $('input[name=user_height]').val() != "") {
+            var weightObj = '[{"date" : "'+new Date()+'","weight" : '+$('input[name=user_weight]').val()+'}]';
+            var heightObj = '[{"date" : "'+new Date()+'","height" : '+$('input[name=user_height]').val()+'}]';
+            localStorage.setItem('MH_user_weight', weightObj);
+            localStorage.setItem('MH_user_height', heightObj);
+            if (localStorage.getItem('MH_user_weight') != null && localStorage.getItem('MH_user_height') != null) {
+                $.mobile.navigate('#profile');
+                $('.profile_user_weight').html($('input[name=user_weight]').val());
+                $('.profile_user_height').html($('input[name=user_height]').val());
+                var weight = $('input[name=user_weight]').val();
+                var height = $('input[name=user_height]').val();
+                if (weight > 0 && height > 0) {
+                    var finalBmi = weight / (height / 100 * height / 100);
+                    var bmiObj = '[{"date" : "'+new Date()+'","bmi" : '+finalBmi.toFixed(2)+'}]';
+                    localStorage.setItem('MH_user_bmi', bmiObj);
+                }
+                $('.profile_user_bmi').html(finalBmi.toFixed(2));
+                location.reload();
+            }
+        }
+        else {
+            alert('Sila lengkapkan pendaftaran anda.');
+        }
+    });
+    $('.profile_picture_name h2').html(localStorage.getItem('MH_user_name'));
+    if (localStorage.getItem('MH_user_gender') == 'male') {
+        $('.profile_user_age img').attr('src', 'images/masculine.png');
+    }
+    else {
+        $('.profile_user_age img').attr('src', 'images/femenine.png');
+    }
+    $('.profile_user_age age').html(localStorage.getItem('MH_user_age'));
+    if (localStorage.getItem('MH_user_weight') != null && localStorage.getItem('MH_user_height') != null) {
+        var weightJSON = JSON.parse(localStorage.getItem('MH_user_weight'));
+        var heightJSON = JSON.parse(localStorage.getItem('MH_user_height'));
+//        var weight = weightJSON[0].weight;
+//        var height = heightJSON[0].height;
+//        if (weight > 0 && height > 0) {
+//            var finalBmi = weight / (height / 100 * height / 100);
+//            var bmiJSON = JSON.parse(localStorage.getItem('MH_user_bmi'));
+//            bmiJSON.unshift({date : ""+new Date(), bmi : finalBmi.toFixed(2)});
+//            localStorage.setItem('MH_user_bmi', JSON.stringify(bmiJSON));
+//        }
+        var bmiJSON = JSON.parse(localStorage.getItem('MH_user_bmi'));
+        $('.profile_user_weight').html(weightJSON[0].weight);
+        $('.profile_user_height').html(heightJSON[0].height);
+        $('.profile_user_bmi').html(bmiJSON[0].bmi);
+        
+        $('ul.weight-list').html("");
+        
+        $.each(weightJSON, function(i,v){
+            var date = v.date;
+            date = date.split(" ");
+            var stats = "unchanged";
+            if (v.weight > v.weight){
+                stats = "increase";
+            }else if (v.weight < v.weight){
+                stast = "drop";
+            }
+            var weightStr = '<li><a href="#"><span class="day_name">'+date[0]+'</span>&nbsp; '+date[1]+' '+date[2]+' <label class="digits"><i> <img src="images/'+stats+'.png" alt="" width="16" height="16"></i> '+v.weight+' <i style="color: #9099B7">kg</i></label><div class="clear"></div></a></li>';
+            $('ul.weight-list').append(weightStr);
+        });
+        
+        $.each(heightJSON, function(i,v){
+            var date = v.date;
+            date = date.split(" ");
+            var stats = "unchanged";
+            if (v.height > v.height){
+                stats = "increase";
+            }else if (v.height < v.height){
+                stast = "drop";
+            }
+            var heightStr = '<li><a href="#"><span class="day_name">'+date[0]+'</span>&nbsp; '+date[1]+' '+date[2]+' <label class="digits"><i> <img src="images/'+stats+'.png" alt="" width="16" height="16"></i> '+v.height+' <i style="color: #9099B7">kg</i></label><div class="clear"></div></a></li>';
+            $('ul.height-list').append(heightStr);
+        });
+        
+        $.each(bmiJSON, function(i,v){
+            var date = v.date;
+            date = date.split(" ");
+            var stats = "unchanged";
+            if (v.bmi > v.bmi){
+                stats = "increase";
+            }else if (v.bmi < v.bmi){
+                stast = "drop";
+            }
+            var bmiStr = '<li><a href="#"><span class="day_name">'+date[0]+'</span>&nbsp; '+date[1]+' '+date[2]+' <label class="digits">healthy <i style="color: #9099B7">'+v.bmi+'</i> <i> <img src="images/unchanged.png" alt="" width="16" height="16"></i></label><div class="clear"></div></a></li>';
+            $('ul.bmi-list').append(bmiStr);
+        });
+        
+    }
     var showChar = 60; // How many characters are shown by default
     var ellipsestext = "...";
     var moretext = "Read more+";
@@ -28,6 +143,41 @@ $(document).ready(function () {
         $(this).prev().toggle();
         return false;
     });
+    $('.update-weight').on('click', function () {
+        var weight = $('input[name=update_user_weight]').val();    
+        var date = new Date();
+        var weightJSON = JSON.parse(localStorage.getItem('MH_user_weight'));
+        weightJSON.unshift({date : ""+new Date(), weight : weight});
+        localStorage.setItem('MH_user_weight', JSON.stringify(weightJSON));
+        
+        var heightJSON = JSON.parse(localStorage.getItem('MH_user_height'));
+        var height = heightJSON[0].height;
+        if (weight > 0 && height > 0) {
+            var finalBmi = weight / (height / 100 * height / 100);
+            var bmiJSON = JSON.parse(localStorage.getItem('MH_user_bmi'));
+            bmiJSON.unshift({date : ""+new Date(), bmi : finalBmi.toFixed(2)});
+            localStorage.setItem('MH_user_bmi', JSON.stringify(bmiJSON));
+        }
+        
+        location.reload();
+    });
+    $('.update-height').on('click', function () {
+        var height = $('input[name=update_user_height]').val();    
+        var date = new Date();
+        var heightJSON = JSON.parse(localStorage.getItem('MH_user_height'));
+        heightJSON.unshift({date : ""+new Date(), height : height});
+        localStorage.setItem('MH_user_height', JSON.stringify(heightJSON));
+        
+        var weightJSON = JSON.parse(localStorage.getItem('MH_user_weight'));
+        var weight = weightJSON[0].weight;
+        if (weight > 0 && height > 0) {
+            var finalBmi = weight / (height / 100 * height / 100);
+            var bmiJSON = JSON.parse(localStorage.getItem('MH_user_bmi'));
+            bmiJSON.unshift({date : ""+new Date(), bmi : finalBmi.toFixed(2)});
+            localStorage.setItem('MH_user_bmi', JSON.stringify(bmiJSON));
+        }
+        location.reload();
+    });
     // assuming you've got the appropriate language files,
     // clndr will respect whatever moment's language is set to.
     // moment.lang('ru');
@@ -50,8 +200,7 @@ $(document).ready(function () {
     // then onMonthChange (if the month changed).
     // finally onYearChange (if the year changed).
     calendars.clndr1 = $('.cal1').clndr({
-        events: eventArray
-        , // constraints: {
+        events: eventArray, // constraints: {
         //   startDate: '2013-11-01',
         //   endDate: '2013-11-15'
         // },
